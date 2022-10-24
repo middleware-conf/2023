3c918d9feef7368b8d366a2bd14e7aa10e6d3d9d
Giuseppe Di Modica <giuseppe.dimodica@unibo.it>
2022-10-24 18:18:46 +0200

    home and CfP

--- js/w3-include-html.js
+++ js/w3-include-html.js
@@ -1,20 +1,27 @@
 function includeHTML() {
-  var z, i, elmnt, file, xhttp;
+  var z, i, elmnt, file, xhttp, attrs, parent;
   /*loop through a collection of all HTML elements:*/
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
+    /*search for elements with a certain attribute:*/
     file = elmnt.getAttribute("w3-include-html");
     if (file) {
       /*make an HTTP request using the attribute value as the file name:*/
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;
+          			        // get the element's parent node
+          			    parent = elmnt.parentNode;
+          				// move all children out of the element
+				    while (elmnt.firstChild) parent.insertBefore(elmnt.firstChild, elmnt);
+					// remove the empty element
+     				    parent.removeChild(elmnt)
+          }
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
           /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
+          //elmnt.removeAttribute("w3-include-html");
           includeHTML();
         }
       }      
